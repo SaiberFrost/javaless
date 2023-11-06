3363d4dbff54d9ef31f60b9b55c73bf1b43dde1d
SaiberFrost <sanji30000@gmail.com>
2023-11-06 20:02:03 +0300

    initial commit

---
+++ src/com/moroz/lesson1/Main.java
@@ -0,0 +1,10 @@
+package com.moroz.lesson1;
+
+public class Main {
+    public static void main(String[] args) {
+        int variable = 199;
+        variable += 5;
+
+
+    }
+}
---
+++ src/com/moroz/lesson1/Task1.java
@@ -0,0 +1,13 @@
+package com.moroz.lesson1;
+
+public class Task1 {
+
+        public static void main(String[] args) {
+            double number = 241.7;
+            double number2 = number % 10;
+
+            System.out.println(number2);
+
+        }
+
+    }
