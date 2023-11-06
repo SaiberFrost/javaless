2747f4c6f4f564bb2bce22d0bf160ba63cc672e8
SaiberFrost <sanji30000@gmail.com>
2023-11-06 20:19:44 +0300

    implemented task2 homework

---
+++ src/com/moroz/homework/Task2.java
@@ -0,0 +1,13 @@
+package com.moroz.homework;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int number = 9999;
+        int lastNumber = number / 100;
+        int firstNumber = number % 100;
+        int allNumber = lastNumber + firstNumber;
+        System.out.println(lastNumber);
+        System.out.println(firstNumber);
+        System.out.println(allNumber);
+    }
+}
