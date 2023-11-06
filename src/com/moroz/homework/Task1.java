24bd26682b9b535976a2cee18e3b39e7a6de01fe
SaiberFrost <sanji30000@gmail.com>
2023-11-06 20:15:21 +0300

    implementing task1 homework

---
+++ src/com/moroz/homework/Task1.java
@@ -0,0 +1,11 @@
+package com.moroz.homework;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        int number = 1999;
+        int lastNumber = number % 10;
+        System.out.println(lastNumber);
+    }
+
+}
