7d96f23bfcebfc5bfc4e46a1a71c6577fe09fd2c
SaiberFrost <sanji30000@gmail.com>
2023-11-13 20:58:42 +0300

    implemented task 8 homework

--- src/com/moroz/homework/Task8.java
+++ src/com/moroz/homework/Task8.java
@@ -2,7 +2,7 @@ package com.moroz.homework;
 
 public class Task8  {
     public static void main(String args[]) {
-        int number = 40;
+        int number = 38;
         if ( number % 2 == 0 )
         System.out.println("Число четное");
         else
